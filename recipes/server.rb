2920f40442f03dde9f7485447511ab94a6df8fc5
arnabsen1234 <arnabsen1234@gmail.com>
2017-10-05 17:28:33 +0000

    changes in Apache recipe using the template

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,11 +8,10 @@ package 'httpd' do
   action :install
 end 
 
-file '/var/www/html/index.html' do
-  content "<html><h1>Hello World</h1>
-  <h2>IP Address is #{node['ipaddress']}</h2>
-  <h2>Node Name is #{node['hostname']}</h2>
-</html>"
+template '/var/www/html/index.html' do
+
+ source 'index.html.erb'
+ action :create
 end 
 
 
